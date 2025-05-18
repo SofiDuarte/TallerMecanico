53b07578490db5c14de138ad80e2550876d73310
Sofi DUarte <sofiduvi@gmail.com>
2025-05-18 00:21:04 -0300

    Cambio en mecanico por que deje dos veces el navegador y el pie

--- mecanico.php
+++ mecanico.php
@@ -8,18 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-         <div class="encabezado">
-        <head class="navegacion">
-            
-            
-            <a  href="http://localhost/tallermecanico/inicio.php"><img class="login" src="iconos/WA_Sport.jpg" alt="Logotipo de WA Sport" ></a>
-            <a class="nav" href="http://localhost/tallermecanico/nosotros.php">Nosotros</a>
-            <a class="nav" href="http://localhost/tallermecanico/servicios.php">Servicios</a>
-            <a class="nav" href="http://localhost/tallermecanico/contacto.php">Contacto</a>
-            <a class="nav" href="http://localhost/tallermecanico/login.php">Login</a>
-            
-        </head>
-    </div>
+        
     <?php 
         include("navegador.php");
     ?>
@@ -82,18 +71,7 @@
     <?php 
         include("piedepagina.php");
     ?>
-     <footer class="PieDePagina"> 
-        <h4 class="redes">Redes</h4>
-    <div class="iconos-redes">
-        <link rel="stylesheet" href=""><img class= "iconos" src="iconos/face.webp" alt="icono de Facebook con link a la pagina">
-        <link rel="stylesheet" href=""><img class= "iconos"src="iconos/inst.png" alt="icono de Instragram con link a la pagina">
-        <link rel="stylesheet" href=""><img class= "iconoswpp" src="iconos/wpp.png" alt="icono de Whatsapp con link a la pagina">
-
-       
-
-    </div>
-        <a class="direc" href="https://www.google.com.ar/maps/place/WAsport/@-34.6485696,-58.5412355,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcc94f14768b4b:0x569b8550c525f535!8m2!3d-34.6485696!4d-58.5386606!16s%2Fg%2F11h7z1d_5t?hl=es&entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D" >Paso 1418, Ramos Mejía, Provincia de Buenos Aires</a>
-    </footer>
+    
 
 </body>
 </html>
