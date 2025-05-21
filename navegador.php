f84f420a4f5cc9f1f586f8978b8ab0a4748520d5
Christian_Caprarulo <christian.caprarulo@gmail.com>
2025-05-21 10:28:24 -0300

    Arreglo versiones para buscador

--- navegador.php
+++ navegador.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="estilopagina.css">
+    <link rel="stylesheet" href="estilopagina.css?v=<?= time() ?>">
     <title>Navegador </title>
 
 
